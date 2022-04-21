4df9b9bb86fd92a269b7f0bff1af60ecb278efd6
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2022-04-20 20:46:22 -0400

    made things a bit simpler

--- numpy_letters.py
+++ numpy_letters.py
@@ -1,30 +1,22 @@
 import numpy as np
 import random
 
-class NeuralNetwork:
+import cv2
+import os
 
-    # layers in the format of [('activation function', size int)]
-    def __init__ (self, layers):
-        # init weights based on sizes and activations
-        self.layers = layers
-        self.biases = [np.random.randn(y,) for (_, y) in layers]
-        self.weights = [np.random.randn(y, x) for ((_,x), (_,y)) in zip(layers[:-1], layers[1:])]
+# layers in the format of [('activation function', size int)]
+layers = [("sigmoid", 10), ("sigmoid", 10), ("sigmoid", 10)]
+biases = [np.random.randn(y,) for (_, y) in layers]
+weights = [np.random.randn(y, x) for ((_,x), (_,y)) in zip(layers[:-1], layers[1:])]
 
-    def feed(self, input):
-        # feed forward through the network
-        for (b, w, l) in zip(self.biases, self.weights, self.layers):
-            input = self.activation(l[0], np.dot(w, input) + b)
+def activation(activation, input):
+    #switch statement
+    if activation == 'sigmoid':
+        return 1 / (1 + np.exp(-input))
+    elif activation == 'relu':
+        return np.where(input > 0, input, 0)
+    else:
         return input
 
-    def activation(self, activation, input):
-        #switch statement
-        if activation == 'sigmoid':
-            return 1 / (1 + np.exp(-input))
-        elif activation == 'relu':
-            return np.where(input > 0, input, 0)
-        else:
-            return input
 
-
-nn = NeuralNetwork([('sigmoid', 2), ('sigmoid', 2), ('sigmoid', 2)])
-print(nn.feed(np.array([1, 1])))
+print(feed(np.array([1, 1])))
