dc40008adec4e5a0b5f4a48a0fcdcc688324df2e
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2022-05-14 22:18:15 -0400

    possible chrome fix again

--- script.js
+++ script.js
@@ -59,7 +59,9 @@ function predict(input) {
     let labelMap = {
         0: 48, 1: 49, 2: 50, 3: 51, 4: 52, 5: 53, 6: 54, 7: 55, 8: 56, 9: 57, 10: 65, 11: 66, 12: 67, 13: 68, 14: 69, 15: 70, 16: 71, 17: 72, 18: 73, 19: 74, 20: 75, 21: 76, 22: 77, 23: 78, 24: 79, 25: 80, 26: 81, 27: 82, 28: 83, 29: 84, 30: 85, 31: 86, 32: 87, 33: 88, 34: 89, 35: 90, 36: 97, 37: 98, 38: 100, 39: 101, 40: 102, 41: 103, 42: 104, 43: 110, 44: 113, 45: 114, 46: 116,
     };
-    let predictions = arrayEntries(output[0]).sort((a,b)=>a[1] < b[1]);
+    let predictions = Object.entries(output[0]).sort((a,b)=>{
+        return b[1] - a[1]
+    });
     //let letters = predictions.map(x => String.fromCharCode(labelMap[x[0]]))
     let predContainer = document.querySelector("#predictions")
     predContainer.innerHTML = ""
@@ -71,12 +73,6 @@ function predict(input) {
     })
 
 }
-
-
-function arrayEntries(arr){
-    return arr.map((item, index)=>[index, item])
-}
-
 
 //init canvas
 
